05794adf9ddbc0682d95a2b161f8942ae523627e
sub061 <subhash.tripathi@webcontentor.com>
2023-05-30 20:01:46 +0530

    patient form all field align

---
+++ CommonServices/SexualOrientation.cs
@@ -0,0 +1,26 @@
+﻿namespace Medical_Athena_Calendly.CommonServices
+{
+    public class SexualOrientation
+    {
+        public string Name { get; set; }
+    }
+
+    public static class SexualOrientationData
+    {
+        public static List<SexualOrientation> GetData()
+        {
+            var sexualOrientations = new List<SexualOrientation>
+            {
+        new SexualOrientation { Name = "Straight or heterosexual" },
+        new SexualOrientation {  Name = "Lesbian, gay or homosexual" },
+        new SexualOrientation {  Name = "Bisexual" },
+        new SexualOrientation {  Name = "Don't know" },
+        new SexualOrientation {  Name = "Something else" },
+        new SexualOrientation {  Name = "Choose not to disclose" },
+        new SexualOrientation {  Name = "Something else, please describe" }
+             };
+
+            return sexualOrientations;
+        }
+    }
+}
